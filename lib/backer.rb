b87107dded4d347815970dd0f63c1efe41fef21b
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-09 07:54:14 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,12 +2,11 @@
 require 'pry'
 
 class Backer
-  attr_accessor :name, :backed_projects, :backers
+  attr_accessor :name, :backed_projects
 
   def initialize(name)
     @name = name
     @backed_projects = []
-    @backers << self
   end
 
   def back_project(title)
